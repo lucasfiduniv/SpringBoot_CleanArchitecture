7008ca9c3e01b940f880c031fba4159969e0741a
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-10-11 17:57:54 -0300

    feat: :sparkles: criando entidade de certification

---
+++ src/main/java/com/example/java_springboot/modules/students/controllers/entities/CertificationStudentEntity.java
@@ -0,0 +1,13 @@
+package com.example.java_springboot.modules.students.controllers.entities;
+
+import java.util.List;
+import java.util.UUID;
+
+public class CertificationStudentEntity {
+   
+    private UUID id;
+    private UUID studentID;
+    private String technology;
+    private int grate;
+    List<AnswersCertificationsEntity> answersCertificationsEntities;
+}
