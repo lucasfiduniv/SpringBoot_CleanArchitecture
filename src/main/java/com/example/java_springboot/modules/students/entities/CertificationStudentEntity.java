c1698b1e3e2df4905b8f7bfa97ce8f73eec74ea6
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-10-12 11:54:32 -0300

    fix: :bug: configurando os decorators na tabela de certification

--- src/main/java/com/example/java_springboot/modules/students/entities/CertificationStudentEntity.java
+++ src/main/java/com/example/java_springboot/modules/students/entities/CertificationStudentEntity.java
@@ -1,8 +1,19 @@
 package com.example.java_springboot.modules.students.entities;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,11 +21,26 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "certifications")
 public class CertificationStudentEntity {
-   
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    private UUID studentID;
+
+    @Column(length = 100)
     private String technology;
+
+    @Column(length = 10)
     private int grade;
-    List<AnswersCertificationsEntity> answersCertificationsEntities;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", referencedColumnName = "id")
+    private StudentEntity studentEntity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "certification")
+    private List<AnswersCertificationsEntity> answersCertificationsEntities;
 }
