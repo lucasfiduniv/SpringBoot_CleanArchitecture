7a9406a98a9c113b98edc5362b8739f3b659f1f8
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-10-12 09:43:02 -0300

    feat; :sparkles: adicionando os decorators para criar certo no banco de dados

--- src/main/java/com/example/java_springboot/modules/students/entities/StudentyEntity.java
+++ src/main/java/com/example/java_springboot/modules/students/entities/StudentyEntity.java
@@ -3,6 +3,11 @@ package com.example.java_springboot.modules.students.entities;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +15,14 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "students")
 public class StudentyEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(unique = true, nullable = false)
     private String email;
-    private List<CertificationStudentEntity> certificationStudentEntity;
+
+    //private List<CertificationStudentEntity> certificationStudentEntity;
 }
