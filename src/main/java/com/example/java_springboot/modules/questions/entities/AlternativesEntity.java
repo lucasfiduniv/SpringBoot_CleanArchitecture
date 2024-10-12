486961d470d3f588832000dce24bd43a2283d8c6
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-10-12 14:21:35 -0300

    feat: :sparkles: criando entidade de alternativas

---
+++ src/main/java/com/example/java_springboot/modules/questions/entities/AlternativesEntity.java
@@ -0,0 +1,35 @@
+package com.example.java_springboot.modules.questions.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "alternatives")
+public class AlternativesEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column
+    private String description;
+
+    @Column
+    private String isCorrect;
+   
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
