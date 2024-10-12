ca2b2b574116a52091ddf0e9432a17e07072ebb0
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-10-11 21:38:30 -0300

    feat: :sparkles: criando controller de estudate

---
+++ src/main/java/com/example/java_springboot/modules/students/controllers/StudentController.java
@@ -0,0 +1,27 @@
+package com.example.java_springboot.modules.students.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.java_springboot.modules.students.dto.VerifyHasCertificationDTO;
+import com.example.java_springboot.modules.students.useCases.VerifyIfHasCertificationUseCase;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private VerifyIfHasCertificationUseCase verifyIfHasCertificationUseCase;
+
+    @PostMapping("/verifyIfhasCertification")
+    public String verifyIfhasCertification(@RequestBody VerifyHasCertificationDTO verifyHasCertificationDTO) {
+     var result = this.verifyIfHasCertificationUseCase.execute(verifyHasCertificationDTO);
+     if(result) {
+        return "usuario ja fez a prova";
+     }
+        return "usuario nao fez a prova";
+    }
+}
