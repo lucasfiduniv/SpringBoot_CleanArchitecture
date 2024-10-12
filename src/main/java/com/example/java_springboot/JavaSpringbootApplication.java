3b16c1285c1cfb8f9b00a962f49bdb5236351ca7
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-10-12 10:09:15 -0300

    chore: :wrench: importando essas variaveis de ambiente para dentro do arquivo main do java

--- src/main/java/com/example/java_springboot/JavaSpringbootApplication.java
+++ src/main/java/com/example/java_springboot/JavaSpringbootApplication.java
@@ -1,4 +1,6 @@
 package com.example.java_springboot;
+
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -6,7 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaSpringbootApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
+		System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
+		System.setProperty("POSTGRES_DB", dotenv.get("POSTGRES_DB"));
+
 		SpringApplication.run(JavaSpringbootApplication.class, args);
 	}
-
 }
