66fa1e4dbc82a5595391416e8fc8b18f58380750
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-10-12 11:55:28 -0300

    fix: :bug: arrumando usecase para agora lidar com os dados nao chumbados

--- src/main/java/com/example/java_springboot/modules/students/useCases/VerifyIfHasCertificationUseCase.java
+++ src/main/java/com/example/java_springboot/modules/students/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,15 +1,21 @@
 package com.example.java_springboot.modules.students.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.java_springboot.modules.students.dto.VerifyHasCertificationDTO;
+import com.example.java_springboot.modules.students.repositories.CertificationStudentRepository;
 
 @Service
 public class VerifyIfHasCertificationUseCase {
-    
-    public boolean execute(VerifyHasCertificationDTO verifyHasCertificationDTO) {
-        if(verifyHasCertificationDTO.getEmail().equals("lucfiduniv@gmail.com") 
-           && verifyHasCertificationDTO.getTechnology().equals("JAVA")) {
+
+    @Autowired
+    private CertificationStudentRepository certificationStudentRepository;
+
+    public boolean execute(VerifyHasCertificationDTO dto) {
+        var result = this.certificationStudentRepository.findByStudentEmailAndTechnology(dto.getEmail(),
+                dto.getTechnology());
+        if (!result.isEmpty()) {
             return true;
         }
         return false;
