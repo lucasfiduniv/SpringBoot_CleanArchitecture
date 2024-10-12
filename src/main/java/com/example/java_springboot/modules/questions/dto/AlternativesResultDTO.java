54bccc48818aefcee2aff3c88ff9157b023cedee
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-10-12 14:36:34 -0300

    feat: :sparkles: criando dto para alternativas

---
+++ src/main/java/com/example/java_springboot/modules/questions/dto/AlternativesResultDTO.java
@@ -0,0 +1,9 @@
+package com.example.java_springboot.modules.questions.dto;
+
+import java.util.UUID;
+
+public class AlternativesResultDTO {
+    
+    private UUID id;
+    private String description;
+}
