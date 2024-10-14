beb2afc06f64770c115b27c97eb7a4e68cd13034
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-10-14 08:54:36 -0300

    fix: :bug: removendo validação de estudante que nao precisava

--- src/main/java/com/example/java_springboot/modules/students/useCases/StudentCertificationAnswersUseCase.java
+++ src/main/java/com/example/java_springboot/modules/students/useCases/StudentCertificationAnswersUseCase.java
@@ -8,36 +8,24 @@ import org.springframework.stereotype.Service;
 import com.example.java_springboot.modules.questions.entities.QuestionEntity;
 import com.example.java_springboot.modules.questions.repositories.QuestionRepository;
 import com.example.java_springboot.modules.students.dto.StudentCertificationAnswerDTO;
-import com.example.java_springboot.modules.students.repositories.StudentRepository;
 
 @Service
 public class StudentCertificationAnswersUseCase {
     
-    private final StudentRepository studentRepository;
-
-    @Autowired
-    public StudentCertificationAnswersUseCase(StudentRepository studentRepository) {
-        this.studentRepository = studentRepository;
-    }
-
     @Autowired
     private QuestionRepository questionRepository;
 
     public StudentCertificationAnswerDTO execute(StudentCertificationAnswerDTO studentCertificationAnswerDTO) {
-        var student = studentRepository.findByEmail(studentCertificationAnswerDTO.getEmail());
-        
-        if (student.isEmpty()) {
-            throw new RuntimeException("Email não encontrado");
-        }
+     
         List<QuestionEntity> questionsEntity = questionRepository.findByTechnology(studentCertificationAnswerDTO.getTechnology());
         studentCertificationAnswerDTO.getQuestionsAnswers().stream().forEach(questionsAnswer -> {
           var questions =  questionsEntity.stream().filter(question -> question.getId().equals(questionsAnswer.getId())).findFirst().get();
           var findCorrectAlternative = questions.getAlternatives().stream().filter(alternative -> alternative.isCorrect()).findFirst().get();
 
           if (findCorrectAlternative.getId().equals(questionsAnswer.getAlternativeId())) {
-            questionAnswer.setCorrect(false);
+            questionsAnswer.setIsCorrect(true);
           }else{
-            questionAnswer.setCorrect(false);
+            questionsAnswer.setIsCorrect(false);
           } 
         });
         return studentCertificationAnswerDTO;
