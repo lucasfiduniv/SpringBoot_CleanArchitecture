51542389f5663a3beb50e3cfe982abd42a9b7056
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-10-14 13:51:08 -0300

    fix: :bug: refatorando e deixando mais robusta a entidade de AnswersCertifications

--- src/main/java/com/example/java_springboot/modules/students/entities/AnswersCertificationsEntity.java
+++ src/main/java/com/example/java_springboot/modules/students/entities/AnswersCertificationsEntity.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,27 +15,35 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "answers_certifications_students")
+@Entity(name = "answers_certification_students")
+@Builder
 public class AnswersCertificationsEntity {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @ManyToOne
-    @JoinColumn(name = "certification_id", nullable = false)  
-    private CertificationStudentEntity certification;
+    @Column(name = "certification_id")
+    private UUID certificationID;
 
-   
+    @ManyToOne()
+    @JoinColumn(name = "certification_id", insertable = false, updatable = false)
+    @JsonBackReference
+    private CertificationStudentEntity certificationStudentEntity;
+
+    @Column(name = "student_id")
+    private UUID studentID;
+
     @ManyToOne
-    @JoinColumn(name = "student_id", nullable = false)
-    private StudentEntity student;
+    @JoinColumn(name = "student_id", insertable = false, updatable = false)
+    private StudentEntity studentEntity;
 
     @Column(name = "question_id")
     private UUID questionID;
@@ -42,7 +52,7 @@ public class AnswersCertificationsEntity {
     private UUID answerID;
 
     @Column(name = "is_correct")
-    private Boolean isCorrect;
+    private boolean isCorrect;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
