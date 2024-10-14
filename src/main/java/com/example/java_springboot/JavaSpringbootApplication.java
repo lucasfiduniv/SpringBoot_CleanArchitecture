90b3a9fb240bd2778063c6770f4b883807a0d7a0
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-10-14 13:49:07 -0300

    fix: :bug: comentando parte de variaveis de ambiente usando dotenv

--- src/main/java/com/example/java_springboot/JavaSpringbootApplication.java
+++ src/main/java/com/example/java_springboot/JavaSpringbootApplication.java
@@ -1,6 +1,6 @@
 package com.example.java_springboot;
 
-import io.github.cdimascio.dotenv.Dotenv;
+// import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,10 +9,10 @@ public class JavaSpringbootApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
-		System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
-		System.setProperty("POSTGRES_DB", dotenv.get("POSTGRES_DB"));
+		// Dotenv dotenv = Dotenv.load();
+		// System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
+		// System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
+		// System.setProperty("POSTGRES_DB", dotenv.get("POSTGRES_DB"));
 
 		SpringApplication.run(JavaSpringbootApplication.class, args);
 	}
