29a4272001568d6e9bca62586b64f8c2497eff5a
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-10-14 13:51:30 -0300

    fix: :bug: arrumando a entidade de certificationStudent

--- src/main/java/com/example/java_springboot/modules/students/entities/CertificationStudentEntity.java
+++ src/main/java/com/example/java_springboot/modules/students/entities/CertificationStudentEntity.java
@@ -6,6 +6,9 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -36,13 +39,20 @@ public class CertificationStudentEntity {
     @Column(length = 10)
     private int grade;
 
+    @Column(name = "student_id")
+    private UUID studentID;
+
     @ManyToOne
-    @JoinColumn(name = "student_id", referencedColumnName = "id")
+    @JoinColumn(name = "student_id", insertable = false, updatable = false)
+
     private StudentEntity studentEntity;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "answer_certification_id", insertable = false, updatable = false)
+    @JsonManagedReference
+    List<AnswersCertificationsEntity> answersCertificationsEntities;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
 
-    @OneToMany(mappedBy = "certification")
-    private List<AnswersCertificationsEntity> answersCertificationsEntities;
 }
