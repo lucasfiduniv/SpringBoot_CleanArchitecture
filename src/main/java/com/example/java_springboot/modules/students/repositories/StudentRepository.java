1ca426c102f0ae4e152461ef2c165cd601f59533
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-10-13 22:21:04 -0300

    fix: :bug: adicionando que o retorno da entidade como opcional para quando nao tiver

--- src/main/java/com/example/java_springboot/modules/students/repositories/StudentRepository.java
+++ src/main/java/com/example/java_springboot/modules/students/repositories/StudentRepository.java
@@ -1,5 +1,6 @@
 package com.example.java_springboot.modules.students.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.java_springboot.modules.students.entities.StudentEntity;
 
 public interface StudentRepository extends JpaRepository<StudentEntity, UUID> {
-    public StudentEntity findByEmail(String email);
+    public Optional<StudentEntity> findByEmail(String email);
 }
